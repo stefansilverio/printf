e3a547f878a85993cad4ce78c3b8c661c8167700
stefansilverio <494@holbertonschool.com>
2018-10-23 00:32:28 +0000

    updated headerfile

--- holberton.h
+++ holberton.h
@@ -17,5 +17,6 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list ap);
 int print_string(va_list ap);
+int call_print_fn(char ch, print_t funcs[], va_list ap);
 
 #endif
