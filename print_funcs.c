b1ffe500a38a28d2d9c3951f52a7f4fdb7992c67
stefansilverio <494@holbertonschool.com>
2018-10-23 18:11:27 +0000

    added print_int and print_digit functions to handle integers

--- print_funcs.c
+++ print_funcs.c
@@ -30,3 +30,48 @@ int print_string(va_list ap)
 	return (count);
 }
 
+/**
+ * print_int - prints a digit from va_list
+ * @ap: va_list object from calling function
+ * Return: integer count of characters printed
+ */
+int print_int(va_list ap)
+{
+	int *count_ptr;
+	int num;
+
+	count_ptr = malloc(sizeof(int));
+
+	if (!count_ptr)
+		exit(-1);
+
+	*count_ptr = 0;
+	num = va_arg(ap, int);
+
+	(*count_ptr) = print_digit(num, count_ptr);
+
+	return (*count_ptr);
+}
+
+/**
+ * print_digit - print the digits recursively
+ * @num: next num in the va_arg list
+ * @count: count digits
+ * Return: Always 0
+ */
+int print_digit(int num, int *count)
+{
+	if (num < 0)
+	{
+		(*count) += _putchar('-');
+		num = -num;
+	} /* handle int min & max */
+
+	if (num / 10)
+	{
+		print_digit(num / 10, count);
+	}
+
+	(*count) += _putchar((num % 10) + '0');
+	return (*count);
+}
