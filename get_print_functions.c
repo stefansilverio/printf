19077661e5c5bf3501c0b48d7689bc670e04e12d
stefansilverio <494@holbertonschool.com>
2018-10-23 00:18:14 +0000

    implemented call print funcs

---
+++ get_print_functions.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * call_print_func - call appropriate print fn
+ * @format: format string character
+ * @funcs: arrays of structures and functions
+ * @va_list: object to be printed
+ * Return: number of characters printed
+ */
+int call_print_func(char ch, print_t funcs, va_list ap)
+{
+	int j;
+	int count = 0;
+
+	for (j = 0; funcs[j].spec != NULL; j++)
+	{
+		if (ch == funcs[j].spec[0])
+		{
+			count += funcs[j].fn(ap);
+			break;
+		}
+	}
+	return (count);
+}
