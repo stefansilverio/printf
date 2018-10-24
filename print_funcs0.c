b7eb51dd2900df25d5138f174799d61ea5ff1fa6
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-24 06:18:35 +0000

    Removed print_space from ...funcs0

--- print_funcs0.c
+++ print_funcs0.c
@@ -27,29 +27,3 @@ int print_string(va_list ap)
 
 	return (count);
 }
-
-int print_space(va_list ap)
-{
-	char ch = va_arg(ap, int);
-	int count;
-	int i;
-
-	while (ch == ' ')
-		ch = va_arg(ap, int);
-
-	while (funcs[i].spec[0] != NULL)
-	{
-		if (ch == funcs[i].specs[0])
-		{
-			count += funcs[i].fn(ap);
-			i++;
-		}
-		else
-		{
-			count += _putchar('%');
-			count += _putchar(' ');
-			count += _putchar(ch);
-		}
-		return (count);
-	}
-}
