4520600f11e92ad15a69949580efcd687d5dda06
stefansilverio <494@holbertonschool.com>
2019-01-26 07:05:11 +0000

    core functionality

---
+++ core_functionality/printf.c
@@ -0,0 +1,52 @@
+#include "holberton.h" /* _putchar */
+
+/**
+ * _printf - prints to stdout according to a format string
+ * @format: constant string containing zero or more directives
+ * Return: int number of characters printed (excluding terminating null-byte)
+ */
+int _printf(const char *format, ...)
+{
+	int i, count = 0;
+	va_list ap;
+	print_t funcs[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"d", print_int},
+		{"i", print_int},
+		{"u", print_unsigned},
+		{"o", print_octal},
+		{NULL, NULL}
+	};
+	va_start(ap, format);
+	for (i = 0; format != NULL && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			count += _putchar(format[i]);
+			continue;
+		}
+		switch (format[++i])
+		{
+		case '%':
+			count += _putchar('%');
+			break;
+		case 'c':
+		case 's':
+		case 'd':
+		case 'i':
+		case 'u':
+		case 'o':
+			count += call_print_fn(format[i], funcs, ap);
+			break;
+		default:
+			if (!format[i])
+				return (-1);
+			count += _putchar('%');
+			count += _putchar(format[i]);
+			break;
+		}
+	}
+	va_end(ap);
+	return (count);
+}
